588f639fd02e27e10af5dc8148cbf5f5011c38a0
Manami Ichikawa <mana@Ms-MacBook-Pro.local>
2017-05-16 17:40:12 +0900

    first commit

---
+++ ios-uikit-uitextfield-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-uikit-uitextfield-demo
+//
+//  Created by Manami Ichikawa on 5/16/17.
+//  Copyright © 2017 Manami Ichikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
