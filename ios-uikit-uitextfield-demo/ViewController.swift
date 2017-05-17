1a861402d001e852306db883039b32bd7b3b7d03
Manami Ichikawa <mana@Ms-MacBook-Pro.local>
2017-05-17 22:56:37 +0900

    Add project

--- ios-uikit-uitextfield-demo/ViewController.swift
+++ ios-uikit-uitextfield-demo/ViewController.swift
@@ -8,17 +8,85 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate{
 
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        textField.delegate = self
+        
+        setupPlaceholder()
+        setupBackgroundColor()
+        setupClearButtonMode()
+}
+
+//MARK:-Option
+    
+    private func setupPlaceholder(){
+        
+        textField.placeholder = "テキストを入力"
+    }
+    
+    private func setupBackgroundColor(){
+        
+        textField.backgroundColor = UIColor(white: 0.9, alpha: 1)
+    }
+    
+    //右側に削除ボタン設置
+    private func setupClearButtonMode(){
+    
+        textField.clearButtonMode = .always;
+    
+    }
+    
+    private func closeKeyBoard(){
+    
+        textField.resignFirstResponder()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+// MARK: UITextFieldDelegate
+    
+    //returnが押された時
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("textFieldShouldReturn")
+        
+        closeKeyBoard()
+        return true
     }
+    //returnが押された時、textFieldShouldReturnの後
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        print("textFieldShouldEndEditing")
+        return true
+    }
+    
+    //returnが押された時、textFieldShouldReturnの後
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("textFieldDidEndEditing")
+    }
+    
+    //buildした時とreturn後、カーソルが当たった時
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print("textFieldShouldBeginEditing")
+        return true
+    }
+    //buildした時とreturn後、カーソルが当たった時textFieldShouldBeginEditingの後
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("textFieldDidBeginEditing")
+    }
+    
+    //設置したclearButtonが押された時に呼ばれる
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        print("textFieldShouldClear")
+        return true
+
+    }
+
+
 
 
 }
